40b4b13252159c15ddf70d4d8652a6583f8e6a1b
Roel Lucassen <roel.lucassen@student.fontys.nl>
2021-06-03 23:21:54 +0200

    Seperated ProfileService

--- Whatsdown-Authentication-Service/Data/AuthenticationContext.cs
+++ Whatsdown-Authentication-Service/Data/AuthenticationContext.cs
@@ -10,7 +10,6 @@ namespace Whatsdown_Authentication_Service.Data
     public class AuthenticationContext : DbContext
     {
         public DbSet<User> Users { get; set; }
-        public DbSet<Profile> Profiles { get; set; }
        
         public AuthenticationContext(DbContextOptions<AuthenticationContext> options) : base(options)
         {
